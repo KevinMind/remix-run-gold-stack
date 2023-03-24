e006e344efded2df60a4e38543f51dc13010a376
Kevin Meinhardt <kmeinhardt7@gmail.com>
2023-03-24 14:45:52 +0000

    chore(): rename app

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Lenzhardts",
+  title: "remix-run-purple-stack",
   viewport: "width=device-width,initial-scale=1",
 });
 
