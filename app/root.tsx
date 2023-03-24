328cc4b260b79fcc6e8d72f34b98f1ac35a22113
Kevin Meinhardt <kmeinhardt7@gmail.com>
2023-03-24 14:46:32 +0000

    tmp: stuff

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "remix-run-purple-stack",
+  title: "remix-purple-stack",
   viewport: "width=device-width,initial-scale=1",
 });
 
